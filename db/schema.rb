3a2e0fad5a09ca64f863c1db609b45f765a5a327
Ryan Graham <r.m.graham@gmail.com>
2012-11-06 00:15:05 -0800

    Passing tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121106074131) do
+ActiveRecord::Schema.define(:version => 20121106081412) do
 
   create_table "checkins", :force => true do |t|
     t.integer  "user_id"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(:version => 20121106074131) do
     t.datetime "locked_at"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "fullname"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
