096fc2d13e6a5e48b7fb54adde3d40be376f8586
Ryan Graham <r.m.graham@gmail.com>
2012-11-06 00:33:32 -0800

    Passing spec

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -1,4 +1,8 @@
 class Checkin < ActiveRecord::Base
   belongs_to :user
   attr_accessible :ending_at, :starting_at
+
+  scope :current, ->() {
+    where('starting_at < NOW() AND ending_at > NOW()')
+  }
 end
