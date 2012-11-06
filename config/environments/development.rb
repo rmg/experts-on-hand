93b8d13260d5185a05dd0f1964cb46936c4f11ce
Ryan Graham <r.m.graham@gmail.com>
2012-11-05 23:37:19 -0800

    Require users to confirm their e-mail address

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,20 @@ ExpertsOnHand::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Because Devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "localhost",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 end
