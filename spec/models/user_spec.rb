aa382c63906e63d4a60c40fa1aab17004bbd0cb9
Ryan Graham <r.m.graham@gmail.com>
2012-11-06 00:12:50 -0800

    Add failing spec for fullname

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,20 +3,45 @@ require 'spec_helper'
 describe User do
   pending "add some examples to (or delete) #{__FILE__}"
 
-  valid_user_attrs = {email: "bob@example.com", password: 'somehash'}
+  let(:minimum_attrs) {
+    {
+      email: "bob@example.com",
+      password: 'somehash'
+    }
+  }
 
   context "email address" do
+    let(:missing_email) { minimum_attrs.reject {|k,v| k == :email} }
+
+    it "isn't a valid user without one" do
+      User.new(missing_email).should_not be_valid
+    end
     it "barfs without one" do
-      missing_email = valid_user_attrs.reject {|k,v| k == :email}
-      bad_user = User.new missing_email
-      bad_user.should_not be_valid
       expect { User.create!(missing_email) }.to raise_error
     end
+
     it "sings with one" do
-      user = User.new valid_user_attrs
-      user.should be_valid
-      expect { User.create!(valid_user_attrs) }.not_to raise_error
+      User.new(minimum_attrs).should be_valid
+    end
+    it "doesn't barf when you get it right" do
+      expect { User.create!(minimum_attrs) }.not_to raise_error
     end
   end
 
+  context "fullname" do
+    let(:fullname) { "Bob T. Builder" }
+    let(:fullname_attrs) {
+      minimum_attrs.update(fullname: fullname)
+    }
+
+    it "can be given a fullname at creation" do
+      User.new(fullname_attrs).fullname.should == fullname
+    end
+
+    it "persists its fullname" do
+      first = User.create(fullname_attrs)
+      second = User.find(first.id)
+      second.fullname.should == fullname
+    end
+  end
 end
