6c900ac43ccbbaa09369737153bbef0aae528e1b
Ryan Graham <r.m.graham@gmail.com>
2012-11-06 20:45:38 -0800

    Passing authentication spec

--- app/controllers/checkin_controller.rb
+++ app/controllers/checkin_controller.rb
@@ -4,4 +4,15 @@ class CheckinController < ApplicationController
   def index
     @current = Checkin.current
   end
+
+  def create
+    @checkin = Checkin.new
+    @checkin.starting = params.fetch :starting_at, Time.now
+    @checkin.ending = params.fetch :ending_at, Time.now
+    @checkin.user = current_user
+    unless @checkin.save
+      flash[:error] = "Sorry, something went wrong when checking you in"
+    end
+    render :index
+  end
 end
