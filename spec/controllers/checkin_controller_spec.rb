b61188c93530bee7e45dea54b54e3e8a80cdb086
Ryan Graham <r.m.graham@gmail.com>
2012-11-06 20:36:47 -0800

    Add some failing specs for authentication

--- spec/controllers/checkin_controller_spec.rb
+++ spec/controllers/checkin_controller_spec.rb
@@ -2,13 +2,39 @@ require 'spec_helper'
 
 describe CheckinController do
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      checkins = FactoryGirl.create_list(:current_checkin, 5)
-      get 'index'
-      assigns(:current).should =~ checkins
-      response.should be_success
+  context "when not logged in" do
+    logout_user
+
+    describe "GET 'index'" do
+      it "renders the login page" do
+        get 'index'
+        response.should redirect_to(new_user_session_path)
+      end
+    end
+    describe "POST 'create'" do
+      pending "renders the login page" do
+        post 'create'
+        response.should redirect_to(new_user_session_path)
+      end
     end
   end
 
+  context "when logged in" do
+    login_user
+
+    describe "GET 'index'" do
+      it "returns http success" do
+        checkins = FactoryGirl.create_list(:current_checkin, 5)
+        get 'index'
+        assigns(:current).should =~ checkins
+        response.should be_success
+      end
+    end
+
+    describe "POST 'create'" do
+      it "should let a user post a checkin" do
+
+      end
+    end
+  end
 end
