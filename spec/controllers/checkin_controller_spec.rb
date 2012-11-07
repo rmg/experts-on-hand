374e45877810a11367c98f035769ec2ae286c72c
Ryan Graham <r.m.graham@gmail.com>
2012-11-06 20:21:07 -0800

    Test checkin controller #index better

--- spec/controllers/checkin_controller_spec.rb
+++ spec/controllers/checkin_controller_spec.rb
@@ -4,7 +4,9 @@ describe CheckinController do
 
   describe "GET 'index'" do
     it "returns http success" do
+      checkins = FactoryGirl.create_list(:current_checkin, 5)
       get 'index'
+      assigns(:current).should =~ checkins
       response.should be_success
     end
   end
--- spec/factories/checkins.rb
+++ spec/factories/checkins.rb
@@ -5,5 +5,9 @@ FactoryGirl.define do
     user
     starting_at "2012-11-05 23:41:31"
     ending_at "2012-11-05 23:41:31"
+    factory :current_checkin do
+      starting_at { 1.hour.ago }
+      ending_at { Time.now + 2.hours }
+    end
   end
 end
